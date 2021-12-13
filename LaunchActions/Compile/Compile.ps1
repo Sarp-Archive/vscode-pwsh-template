62e7138947230be4a847147ba70e4bafcc645891
Sarp Eren EGILMEZ <2348.sarp.egilmez.2006@gmail.com>
2021-12-13 19:08:48 +0300

    Launch actions bug fix

--- LaunchActions/Compile/Compile.ps1
+++ LaunchActions/Compile/Compile.ps1
@@ -1,2 +1,2 @@
 $ProjectName = (get-item . ).Name
-powershell.exe -nologo -noprofile -command "Invoke-PS2EXE .\src\${ProjectName}.ps1 .\bin\${ProjectName}.exe"
+powershell.exe -nologo -noprofile -command "Invoke-PS2EXE .\src\Main.ps1 .\bin\${ProjectName}.exe"
